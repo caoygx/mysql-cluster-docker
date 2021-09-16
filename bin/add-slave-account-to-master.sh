c187fb7bf8f2e73c0938a4f63ac0f837b61f9796
junfei <junfei@qq.com>
2021-09-16 19:18:16 +0800

    mysql8 主从

--- bin/add-slave-account-to-master.sh
+++ bin/add-slave-account-to-master.sh
@@ -4,6 +4,6 @@ passwd=$(sed '/MASTER_PASSWD/!d;s/.*=//' ../.env);
 replname=$(sed '/REPL_NAME/!d;s/.*=//' ../.env);
 replpasswd=$(sed '/REPL_PASSWD/!d;s/.*=//' ../.env);
 
-docker exec -it  mysql-master mysql -uroot -p${passwd} \
--e "CREATE USER '${replname}'@'%' IDENTIFIED BY '${replpasswd}' REQUIRE SSL;" \
+docker exec -it  mysql8-master mysql -uroot -p${passwd} \
+-e "CREATE USER '${replname}'@'%' IDENTIFIED WITH mysql_native_password BY '${replpasswd}' REQUIRE SSL;" \
 -e "GRANT REPLICATION SLAVE ON *.* TO 'replicas'@'%';"
